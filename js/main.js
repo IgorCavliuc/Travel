95817dc3539c856683e770bf4b4f7d397724020a
Cavliuc <cavliuc.sh@inbox.ru>
2020-12-29 19:03:36 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+	$('.photo_slider').slick({
+		prevArrow:'<button type="button" class="slick-prev"><img src="img/left_icon-icons.com_66714.svg" width="20"></button>',
+
+		nextArrow:'<button type="button" class="slick-next"><img src="img/right_icon-icons.com_66709.svg"width="20"></button>'
+
+
+	});
+});
